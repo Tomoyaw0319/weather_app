94e1529940c91042eb69e48d8b5bededd9fca10f
Tomoya <tomoyaw0319@gmail.com>
2025-08-01 21:03:16 +0900

    気温と天気の所得

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,27 @@
+import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view(['GET'])
 def get_weather(request):
     city = request.GET.get('city', 'Tokyo')  # クエリパラメータから都市名を取得
-    data = {
+    api_key = '5645e7b257bdb1ff50ee6a8c8d6a37f9'
+
+    # OpenWeatherMap APIのURL
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ja'
+
+    # APIへリクエストを送る
+    res = requests.get(url)
+    data = res.json()
+
+    if res.status_code != 200:
+        return Response({'error': '天気情報の取得に失敗しました'}, status=400)
+
+    # 必要な情報だけ取り出す
+    result = {
         'city': city,
-        'temperature': '25℃',
-        'condition': 'Sunny'
+        'temperature': data['main']['temp'],           # 気温(℃)
+        'condition': data['weather'][0]['description'] # 天気の説明（日本語）
     }
-    return Response(data)
+
+    return Response(result)
